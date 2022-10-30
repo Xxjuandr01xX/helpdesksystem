8e759cdbedb1da32b764ae329b03885078cb4047
Juan Diego Rincon Urdaneta <jd.rincon021@gmail.com>
2022-10-30 17:47:10 -0400

    ultimas validaciones para sistema de soporte tecnico db

--- helpdesk.sql
+++ helpdesk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 18-10-2022 a las 03:15:00
+-- Tiempo de generación: 30-10-2022 a las 21:46:30
 -- Versión del servidor: 8.0.30
 -- Versión de PHP: 8.1.10
 
@@ -31,15 +31,17 @@ CREATE TABLE `hlp_nacionalidades` (
   `id` int NOT NULL,
   `cod_nac` varchar(3) COLLATE utf8mb3_spanish_ci NOT NULL,
   `cod_tel` varchar(10) COLLATE utf8mb3_spanish_ci NOT NULL,
-  `descripcion` varchar(10) COLLATE utf8mb3_spanish_ci NOT NULL
+  `descripcion` varchar(10) COLLATE utf8mb3_spanish_ci NOT NULL,
+  `cod` varchar(3) COLLATE utf8mb3_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `hlp_nacionalidades`
 --
 
-INSERT INTO `hlp_nacionalidades` (`id`, `cod_nac`, `cod_tel`, `descripcion`) VALUES
-(1, 'VEN', '+058', 'VELEZUELA');
+INSERT INTO `hlp_nacionalidades` (`id`, `cod_nac`, `cod_tel`, `descripcion`, `cod`) VALUES
+(1, 'VEN', '+058', 'VELEZUELA', 'V.-'),
+(2, 'COL', '+044', 'COLOMBIA', 'C.-');
 
 -- --------------------------------------------------------
 
@@ -65,7 +67,9 @@ CREATE TABLE `hlp_personas` (
 
 INSERT INTO `hlp_personas` (`id`, `id_nacionalidad_fk`, `dni`, `nombre`, `apellido`, `correo`, `telefono`, `direccion`, `fec_nac`) VALUES
 (1, 1, 'V-23445978', 'JUAN DIEGO', 'RINCON URANETA', 'jd.rincon021@gmail.com', '04146801859', 'AV LA LIMPIA CALLE 79E', '1994-12-13'),
-(2, 1, 'V-24734747', 'LUIS ARTURO', 'RINCON URDANETA', 'luisgmail.com', '1414000000', 'AV DELICIAS', '2022-10-10');
+(4, 1, 'V.-24734747', 'LUIS', 'MEDINA', 'luis@gmail.com', '0414-0000000', '                                                              ESTO ES UNA PRUEBA DE INSERCION DE DATOS                                        \r\n                                                                                              ', '2022-10-31'),
+(5, 1, 'V.-24734747', 'LUIS', 'RINCON', 'luis@gmail.com', '0414-6801859', '           AV LAS DELICIAS                                        \r\n                                               ', '1997-01-16'),
+(6, 1, '', 'MARCOS', 'MEDINA', 'marcos@gmail.com', '0414-6000000', '                       AV BELLA VISTA SANTA LUCIA                          \r\n                                               ', '2022-10-30');
 
 -- --------------------------------------------------------
 
@@ -122,6 +126,7 @@ INSERT INTO `hlp_ticket` (`id`, `codigo`, `titulo`, `descripcion`, `id_usuario_s
 
 CREATE TABLE `hlp_ticket_historico` (
   `id` int NOT NULL,
+  `username` varchar(30) COLLATE utf8mb3_spanish_ci NOT NULL,
   `id_ticket_fk` int NOT NULL,
   `fech_modi` date NOT NULL,
   `hora_modi` time NOT NULL,
@@ -133,8 +138,9 @@ CREATE TABLE `hlp_ticket_historico` (
 -- Volcado de datos para la tabla `hlp_ticket_historico`
 --
 
-INSERT INTO `hlp_ticket_historico` (`id`, `id_ticket_fk`, `fech_modi`, `hora_modi`, `id_status_fk`, `observacion`) VALUES
-(1, 2, '0000-00-00', '838:59:59', 3, 'ACTUALIZACION DE INFORMACION');
+INSERT INTO `hlp_ticket_historico` (`id`, `username`, `id_ticket_fk`, `fech_modi`, `hora_modi`, `id_status_fk`, `observacion`) VALUES
+(1, 'admin', 2, '0000-00-00', '838:59:59', 3, 'ACTUALIZACION DE INFORMACION'),
+(2, 'admin', 2, '2022-10-18', '838:59:59', 3, 'PRUEBA DE INSERCION DE UNA OBSERVACION (1)                                 \r\n                                                    ');
 
 -- --------------------------------------------------------
 
@@ -167,16 +173,19 @@ CREATE TABLE `hlp_usuarios` (
   `id` int NOT NULL,
   `usuario` varchar(80) COLLATE utf8mb3_spanish_ci NOT NULL,
   `passwd` varchar(100) COLLATE utf8mb3_spanish_ci NOT NULL,
-  `id_rol_fk` int NOT NULL
+  `id_rol_fk` int NOT NULL,
+  `id_persona_fk` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `hlp_usuarios`
 --
 
-INSERT INTO `hlp_usuarios` (`id`, `usuario`, `passwd`, `id_rol_fk`) VALUES
-(1, 'admin', 'admin', 1),
-(2, 'lrincon', '1234', 2);
+INSERT INTO `hlp_usuarios` (`id`, `usuario`, `passwd`, `id_rol_fk`, `id_persona_fk`) VALUES
+(1, 'admin', 'admin', 1, 1),
+(3, 'LMEDINA', '1234567', 2, 4),
+(6, 'LRINCON', '123456', 3, 5),
+(7, 'MMEDINA', '123456', 2, 6);
 
 --
 -- Índices para tablas volcadas
@@ -232,13 +241,13 @@ ALTER TABLE `hlp_usuarios`
 -- AUTO_INCREMENT de la tabla `hlp_nacionalidades`
 --
 ALTER TABLE `hlp_nacionalidades`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `hlp_personas`
 --
 ALTER TABLE `hlp_personas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `hlp_roles`
@@ -256,7 +265,7 @@ ALTER TABLE `hlp_ticket`
 -- AUTO_INCREMENT de la tabla `hlp_ticket_historico`
 --
 ALTER TABLE `hlp_ticket_historico`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `hlp_ticket_status`
@@ -268,7 +277,7 @@ ALTER TABLE `hlp_ticket_status`
 -- AUTO_INCREMENT de la tabla `hlp_usuarios`
 --
 ALTER TABLE `hlp_usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
